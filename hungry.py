8afd09ce6a84fd8be3d4ad81f91af9d56cb31f79
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2018-10-18 10:14:05 +0900

    ファイルの削除

--- hungry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-def say_hungry():
-    print("I'm very Hungry!!!")
-
-# main として実行される場合のみ実行する(importの場合は実行しない)
-if __name__ == "__main__":
-    print("main start!")
